4d0b5cd4324b5c88975a40f953a9b30d3a5df052
AminHossyn <mr.aminasady00@gmail.com>
2023-05-24 18:20:44 +0330

    It's finished :)

--- src/component/FilterCars.jsx
+++ src/component/FilterCars.jsx
@@ -1,17 +1,29 @@
+import { useEffect } from "react";
+
 import { BiSort } from "react-icons/bi";
 import { white } from "tailwindcss/colors";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 // class repeat
-const selectClass="w-20 md:w-28 bg-transparent border-transparent focus:ring-0 focus:border-none text-gray-500 text-sm rounded-lg p-0";
-const labelClass="block my-2 text-sm md:text-base font-bold text-gray-900";
-
-
+const selectClass =
+  "w-20 md:w-28 bg-transparent border-transparent focus:ring-0 focus:border-none text-gray-500 text-sm rounded-lg p-0";
+const labelClass = "block my-2 text-sm md:text-base font-bold text-gray-900";
 
 const FilterCars = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <section className="max-w-[1640px] max-auto px-6 py-4 md:px-16 flex flex-col lg:flex-row justify-between items-center gap-2">
       {/* Pick - Up */}
-      <div className="bg-white rounded-md px-2 py-2 md:px-7 md:py-5">
+      <div
+        className="bg-white rounded-md px-2 py-2 md:px-7 md:py-5"
+        data-aos="fade-right"
+        data-aos-duration="1000"
+      >
         <div className="flex items-center">
           <input
             id="helper-radio"
@@ -24,46 +36,60 @@ const FilterCars = () => {
         </div>
         {/* selection */}
         <div className="flex mt-4">
-            <div className="border-r-2 border-gray-200">
-                <label for="location" class={labelClass}>Locations</label>
-                    <select id="location" class={selectClass}>
-                        <option selected>City</option>
-                        <option value="US">United States</option>
-                        <option value="CA">Canada</option>
-                        <option value="FR">France</option>
-                        <option value="DE">Germany</option>
-                    </select>
-            </div>
-            <div className="border-r-2 border-gray-200 ml-2 md:ml-4">
-                <label for="date" class={labelClass}>Date</label>
-                    <select id="date" class={selectClass}>
-                        <option selected>Date</option>
-                        <option value="US">United States</option>
-                        <option value="CA">Canada</option>
-                        <option value="FR">France</option>
-                        <option value="DE">Germany</option>
-                    </select>
-            </div>
-            <div className="ml-2 md:ml-4">
-                <label for="time" class={labelClass}>Time</label>
-                    <select id="time" class={selectClass}>
-                        <option selected>Time</option>
-                        <option value="US">United States</option>
-                        <option value="CA">Canada</option>
-                        <option value="FR">France</option>
-                        <option value="DE">Germany</option>
-                    </select>
-            </div>
+          <div className="border-r-2 border-gray-200">
+            <label for="location" class={labelClass}>
+              Locations
+            </label>
+            <select id="location" class={selectClass}>
+              <option selected>City</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+          </div>
+          <div className="border-r-2 border-gray-200 ml-2 md:ml-4">
+            <label for="date" class={labelClass}>
+              Date
+            </label>
+            <select id="date" class={selectClass}>
+              <option selected>Date</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+          </div>
+          <div className="ml-2 md:ml-4">
+            <label for="time" class={labelClass}>
+              Time
+            </label>
+            <select id="time" class={selectClass}>
+              <option selected>Time</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+          </div>
         </div>
       </div>
 
       {/* Change button */}
-      <button className="bg-sky-600 w-12 h-12 md:w-16 md:h-16 flex items-center justify-center rounded-lg">
-        <BiSort size={35} color={white}/>
+      <button
+        data-aos="fade-up"
+        data-aos-duration="3000"
+        className="bg-sky-600 w-12 h-12 md:w-16 md:h-16 flex items-center justify-center rounded-lg"
+      >
+        <BiSort size={35} color={white} />
       </button>
 
       {/* Drop - Off */}
-      <div className="bg-white rounded-md px-4 py-4 md:px-7 md:py-5">
+      <div
+        className="bg-white rounded-md px-4 py-4 md:px-7 md:py-5"
+        data-aos="fade-left"
+        data-aos-duration="2000"
+      >
         <div className="flex items-center">
           <input
             id="helper-radio"
@@ -76,36 +102,42 @@ const FilterCars = () => {
         </div>
         {/* selection */}
         <div className="flex mt-4">
-            <div className="border-r-2 border-gray-200">
-                <label for="location" class={labelClass}>Locations</label>
-                    <select id="location" class={selectClass}>
-                        <option selected>City</option>
-                        <option value="US">United States</option>
-                        <option value="CA">Canada</option>
-                        <option value="FR">France</option>
-                        <option value="DE">Germany</option>
-                    </select>
-            </div>
-            <div className="border-r-2 border-gray-200 ml-2 md:ml-4">
-                <label for="date" class={labelClass}>Date</label>
-                    <select id="date" class={selectClass}>
-                        <option selected >Date</option>
-                        <option value="US">United States</option>
-                        <option value="CA">Canada</option>
-                        <option value="FR">France</option>
-                        <option value="DE">Germany</option>
-                    </select>
-            </div>
-            <div className="ml-2 md:ml-4">
-                <label for="time" class={labelClass}>Time</label>
-                    <select id="time" class={selectClass}>
-                        <option selected>Time</option>
-                        <option value="US">United States</option>
-                        <option value="CA">Canada</option>
-                        <option value="FR">France</option>
-                        <option value="DE">Germany</option>
-                    </select>
-            </div>
+          <div className="border-r-2 border-gray-200">
+            <label for="location" class={labelClass}>
+              Locations
+            </label>
+            <select id="location" class={selectClass}>
+              <option selected>City</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+          </div>
+          <div className="border-r-2 border-gray-200 ml-2 md:ml-4">
+            <label for="date" class={labelClass}>
+              Date
+            </label>
+            <select id="date" class={selectClass}>
+              <option selected>Date</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+          </div>
+          <div className="ml-2 md:ml-4">
+            <label for="time" class={labelClass}>
+              Time
+            </label>
+            <select id="time" class={selectClass}>
+              <option selected>Time</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+          </div>
         </div>
       </div>
     </section>
--- src/component/PopularCar.jsx
+++ src/component/PopularCar.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import { FaRegHeart } from "react-icons/fa";
 import {
   BsEvStationFill,
@@ -9,10 +13,18 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 const PopularCar = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <section className="max-w-[1640px] max-auto px-6 py-4 md:px-16 ">
       {/* title and view all*/}
-      <div className="flex justify-between px-5 md:px-8 mb-8">
+      <div
+        className="flex justify-between px-5 md:px-8 mb-8"
+        data-aos="fade-up"
+        data-aos-duration="2000"
+      >
         <p className="font-semibold text-sm sm:text-base text-secondary-300">
           Popular Cars
         </p>
@@ -44,57 +56,60 @@ const PopularCar = () => {
           },
         }}
       >
-        
-          {PopularCars.map((item, index) => (
-            <SwiperSlide>
-              <div key={index} className="p-6 rounded-md bg-white shadow-sm">
-                <div className="flex justify-between">
-                  <p className="font-bold text-xl">{item.name}</p>
-                  <span className="cursor-pointer">
-                    <FaRegHeart />
-                  </span>
-                </div>
-                <p className="font-bold text-sm text-secondary-300">
-                  {item.type}
-                </p>
-                <div className="w-full flex items-center justify-center">
-                  <img
-                    src={item.image}
-                    alt={item.name}
-                    srcset=""
-                    className="mt-14 mb-14"
-                  />
-                </div>
+        {PopularCars.map((item, index) => (
+          <SwiperSlide>
+            <div
+              key={index}
+              className="p-6 rounded-md bg-white shadow-sm"
+              data-aos="fade-up"
+              data-aos-duration="2000"
+            >
+              <div className="flex justify-between">
+                <p className="font-bold text-xl">{item.name}</p>
+                <span className="cursor-pointer">
+                  <FaRegHeart />
+                </span>
+              </div>
+              <p className="font-bold text-sm text-secondary-300">
+                {item.type}
+              </p>
+              <div className="w-full flex items-center justify-center">
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  srcset=""
+                  className="mt-14 mb-14"
+                />
+              </div>
 
-                {/* ----------- */}
-                <div className="flex justify-between items-center gap-3 text-secondary-300">
-                  <div className="flex">
-                    <BsEvStationFill />
-                    <span className="font-medium text-sm ml-1.5">90L</span>
-                  </div>
-                  <div className="flex items-center">
-                    <BsRecord2Fill size={20} />
-                    <span className="font-medium text-sm ml-1.5">Manual</span>
-                  </div>
-                  <div className="flex">
-                    <BsFillPeopleFill />
-                    <span className="font-medium text-sm ml-1.5">2 People</span>
-                  </div>
+              {/* ----------- */}
+              <div className="flex justify-between items-center gap-3 text-secondary-300">
+                <div className="flex">
+                  <BsEvStationFill />
+                  <span className="font-medium text-sm ml-1.5">90L</span>
                 </div>
-                {/* ---------- */}
-                <div className="flex justify-between items-center mt-6">
-                  <p className="font-bold text-base">
-                    $99.00/{" "}
-                    <span className="text-sm text-secondary-300">day</span>
-                  </p>
-                  <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded duration-300">
-                    Ren Now
-                  </button>
+                <div className="flex items-center">
+                  <BsRecord2Fill size={20} />
+                  <span className="font-medium text-sm ml-1.5">Manual</span>
+                </div>
+                <div className="flex">
+                  <BsFillPeopleFill />
+                  <span className="font-medium text-sm ml-1.5">2 People</span>
                 </div>
               </div>
-            </SwiperSlide>
-          ))}
-        
+              {/* ---------- */}
+              <div className="flex justify-between items-center mt-6">
+                <p className="font-bold text-base">
+                  $99.00/{" "}
+                  <span className="text-sm text-secondary-300">day</span>
+                </p>
+                <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded duration-300">
+                  Ren Now
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </section>
   );
